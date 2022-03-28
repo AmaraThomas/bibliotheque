681477148d6f5fb0e6c73a332ac4533b2d503c6a
damien lasson <damien.lasson@hotmail.fr>
2022-03-28 12:36:37 +0200

    2eme commit

---
+++ Exo_biblioteque/Form1.cs
@@ -0,0 +1,59 @@
+﻿using Exo_biblioteque.models;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Exo_biblioteque
+{
+    public partial class Form1 : Form
+    {
+        List<Livre> listLivre = new List<Livre>();
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void buttonSave_Click(object sender, EventArgs e)
+        {
+
+
+            string titre = richTextBox1.Text;
+            int isbn = Convert.ToInt32(richTextBox2.Text);
+            Livre livre = new Livre();
+
+            livre.Titre = titre;
+            livre.Isbn = isbn;
+
+            ListViewItem item = new ListViewItem(livre.Titre);
+            item.SubItems.Add(livre.Isbn.ToString());
+            listView1.Items.Add(item);
+
+            listLivre.Add(livre);
+
+        }
+
+        private void listView1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            try
+            {
+                var selectedItem = (Livre)listView1.SelectedItems[0].Tag;
+                if (selectedItem != null)
+                {
+                    MessageBox.Show(selectedItem.ToString(), "détails du livre", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                }    
+                
+            }
+            catch (Exception ex)
+            {
+                //bonjour
+            }
+        }
+    }
+}
